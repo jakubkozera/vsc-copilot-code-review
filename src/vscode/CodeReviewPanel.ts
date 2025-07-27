1c965bf4c78b0e1e74d5f596886ba4ac5524c35c
Jakub Kozera <kozerakuba@gmail.com>
2025-07-27 14:43:02 +0200

    Enhance UI: Add icons to section headers and improve layout for better visual alignment

--- src/vscode/CodeReviewPanel.ts
+++ src/vscode/CodeReviewPanel.ts
@@ -463,7 +463,18 @@ export class CodeReviewPanel implements vscode.WebviewViewProvider {
 <body>
     <div class="container">
         <div class="section">
-            <h3>Branch Comparison</h3>
+            <h3>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-git-pull-request">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M6 18m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                    <path d="M6 6m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                    <path d="M18 18m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                    <path d="M6 8l0 8" />
+                    <path d="M11 6h5a2 2 0 0 1 2 2v8" />
+                    <path d="M14 9l-3 -3l3 -3" />
+                </svg>
+                Branch Comparison
+            </h3>
             <div class="branch-selector">
                 <div class="branch-row">
                     <button id="baseBranch" class="branch-button">
@@ -508,7 +519,15 @@ export class CodeReviewPanel implements vscode.WebviewViewProvider {
         </div>
 
         <div class="section hidden" id="previewSection">
-            <h3>Files to Review</h3>
+            <h3>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-files">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M15 3v4a1 1 0 0 0 1 1h4" />
+                    <path d="M18 17h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h4l5 5v7a2 2 0 0 1 -2 2z" />
+                    <path d="M16 17v2a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h2" />
+                </svg>
+                Files to Review
+            </h3>
             <div id="previewFiles" class="file-preview">
                 <!-- File preview will be populated here -->
             </div>
@@ -525,7 +544,19 @@ export class CodeReviewPanel implements vscode.WebviewViewProvider {
         </div>
 
         <div class="section hidden" id="resultsSection">
-            <h3>Review Results</h3>
+            <h3>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-report-search">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M8 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h5.697" />
+                    <path d="M18 12v-5a2 2 0 0 0 -2 -2h-2" />
+                    <path d="M8 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+                    <path d="M8 11h4" />
+                    <path d="M8 15h3" />
+                    <path d="M16.5 17.5m-2.5 0a2.5 2.5 0 1 0 5 0a2.5 2.5 0 1 0 -5 0" />
+                    <path d="M18.5 19.5l2.5 2.5" />
+                </svg>
+                Review Results
+            </h3>
             <div id="reviewStatus" class="review-status hidden">
                 <div class="spinner"></div>
                 <span id="reviewStatusText">Starting review...</span>
